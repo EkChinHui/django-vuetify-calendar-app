8611256df30adb72bed00c09d4b787ff5a49c541
ekchinhui <ekchinhui@gmail.com>
2021-06-16 11:09:30 +0800

    Setup deployment to heroku

---
+++ cal/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="")
+    start = models.DateField()
+    end = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def getJsonValues(self):
+        eventData = {"name": self.name, "start": self.start, "end": self.end}
+        return eventData
+        
